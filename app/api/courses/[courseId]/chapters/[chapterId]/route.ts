dbb6adc478977f1c7a9be9e8a03c229e8793d9f6
holdennguyen <holdennguyen6174@gmail.com>
2024-11-01 01:52:27 +0700

    Course Publish Action

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -8,7 +8,7 @@ import { db } from "@/lib/db";
 const mux = new Mux({
     tokenId: process.env.MUX_TOKEN_ID, 
     tokenSecret: process.env.MUX_TOKEN_SECRET
-  });
+});
 
 export async function DELETE(
     req: Request,
