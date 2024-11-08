36e0295b81195948933bd31306ef33dcecbdd265
holdennguyen <holdennguyen6174@gmail.com>
2024-11-08 08:39:01 +0700

    Search Query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
+import { Search } from "lucide-react";
+import { SearchInput } from "@/components/search-input";
 
 const SearchPage = async () => {
     const categories = await db.category.findMany({
@@ -7,13 +9,18 @@ const SearchPage = async () => {
             name: "asc",
         }
     });
-    
-    return ( 
-        <div className="p-6">
-            <Categories
-                items={categories}
-            />
-        </div>
+
+    return (
+        <>
+            <div className="px-6 pt-6 md:hidden md:mb-0 block">
+                <SearchInput />
+            </div>
+            <div className="p-6">
+                <Categories
+                    items={categories}
+                />
+            </div>
+        </>
      );
 }
  
